b06562a4f7e537638f1f5adb5366f7163a1d17cf
endink <sharping@outlook.com>
2019-09-09 01:19:50 +0800

    add missed file

---
+++ redis/src/main/kotlin/com/labijie/caching/redis/configuration/RedisRegionOptions.kt
@@ -0,0 +1,18 @@
+package com.labijie.caching.redis.configuration
+
+import com.labijie.caching.redis.serialization.JacksonCacheDataSerializer
+import java.time.Duration
+
+/**
+ * Created with IntelliJ IDEA.
+ * @author Anders Xiao
+ * @date 2019-03-20
+ */
+data class RedisRegionOptions(
+    var url: String = "",
+    var timeout: Duration = Duration.ofSeconds(10),
+    var serializer: String = JacksonCacheDataSerializer.NAME,
+    var password: String = ""
+) {
+
+}
