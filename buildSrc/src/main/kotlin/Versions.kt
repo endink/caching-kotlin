2f76ec62e9e0df44a03afdf92533af0dab6e865f
endink <sharping@outlook.com>
2021-12-10 13:25:47 +0800

    fix buildSrc file missing

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,12 @@
+/**
+ *
+ * @Author: Anders Xiao
+ * @Date: 2021/12/8
+ * @Description:
+ */
+object Versions {
+    const val kotlin = "1.6.0"
+    const val infraBom = "2.6.3"
+    const val infraOrm = "1.0.0"
+    const val infraPlugin = "1.0.6"
+}
