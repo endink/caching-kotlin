8ab20d1706c50d11e13a578e2cbe04fc113183e3
endink <sharping@outlook.com>
2025-07-02 23:09:07 +0800

    upgrade infra bom to 3.5.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,8 +5,7 @@
  * @Description:
  */
 object Versions {
-    const val infraBom = "3.5.+"
+    const val infraBom = "3.5.3"
     const val infraPlugin = "2.1.+"
-    const val nativeBuildTool = "0.10.6"
-    const val kotlin = "2.1.21"
+    const val nativeBuildTool = "0.10.+"
 }
